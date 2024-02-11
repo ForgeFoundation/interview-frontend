a51526ae14f51d33365e67d462447a2315472e76
NelsonWang <wangnelson2@gmail.com>
2024-02-11 07:49:18 -0500

    utiols

--- src/utils.js
+++ src/utils.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
 // const BACKEND_URL = 'http://127.0.0.1:8000';
-const BACKEND_URL = 'https://3nuqhxnbzm.us-east-1.awsapprunner.com/';
-
+const BACKEND_URL = 'https://3nuqhxnbzm.us-east-1.awsapprunner.com';
 export const AskFeedback = async ({
     answer,
     user_fid,
